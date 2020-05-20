62840410d49d310a6eb8d4e783a3a0249d518a58
dmitry <dmitry@espressif.com>
2020-05-20 05:28:23 -0400

    Build and install folder changed.

--- build.sh
+++ build.sh
@@ -1,15 +1,14 @@
-dir
+cd opencv
 mkdir build
 cd build
-dir
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
-    -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules \
-    -DCMAKE_INSTALL_PREFIX=../install \
+    -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
+    -DCMAKE_INSTALL_PREFIX=../../install \
     -DBUILD_TESTS=0 -DBUILD_PERF_TESTS=0 \
     -DBUILD_ZLIB=1 -DWITH_TIFF=1 -DWITH_OPENMP=0 \
     -DWITH_OPENCL=0 -DWITH_OPENGL=0 \
-    -DCMAKE_TOOLCHAIN_FILE=cmake/toolchain-esp32.cmake \
+    -DCMAKE_TOOLCHAIN_FILE=../../cmake/toolchain-esp32.cmake \
     -DTARGET=esp32 -DOPENCV_FORCE_3RDPARTY_BUILD=1 \
     -DWITH_PROTOBUF=0 -DWITH_ADE=0 \
     -I $IDF_PATH/components/vfs/include/sys \
@@ -24,7 +23,8 @@ cmake \
     -DBUILD_opencv_xfeatures2d=0 \
     -DBUILD_opencv_bgsegm=0 \
     -DBUILD_opencv_videoio=0 -DBUILD_SHARED_LIBS=0 \
-    -DBUILD_PACKAGE=1 -DBUILD_opencv_apps=0 ../opencv
+    -DBUILD_PACKAGE=1 -DBUILD_opencv_apps=0 \
+    ../../opencv
 dir
 make -j2
 make install
